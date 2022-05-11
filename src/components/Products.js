75d68b0b4977840494638ca19a2e8354d1b1c93e
Sergen <dmrsrgn@gmail.com>
2022-05-11 18:10:41 -0500

    implement filtering products

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,18 +4,30 @@ import "../App.css";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [filter, setFilter] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getProducts = async () => {
       const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      setProducts(data);
+      if (true) {
+        setProducts(await response.clone().json());
+        setFilter(await response.json());
+      }
       setIsLoading(false);
     };
 
     getProducts();
   }, []);
+
+  const filterData = (category) => {
+    setIsLoading(true);
+    const filteredList = products.filter(
+      (product) => product.category === category
+    );
+    setFilter(filteredList);
+    setIsLoading(false);
+  };
 
   if (isLoading) {
     return <div className="loading__container">Loading...</div>;
@@ -24,13 +36,18 @@ const Products = () => {
     <div className="products__outer-container">
       <h1>Products</h1>
       <div className="categories__container">
-        <button>All</button>
-        <button>Women</button>
-        <button>Men</button>
-        <button>Jewellery</button>
+        <button onClick={() => setFilter(products)}>All</button>
+        <button onClick={() => filterData("women's clothing")}>
+          Women's Clothing
+        </button>
+        <button onClick={() => filterData("men's clothing")}>
+          Men's Clothing
+        </button>
+        <button onClick={() => filterData("jewelery")}>Jewelery</button>
+        <button onClick={() => filterData("electronics")}>Electronics</button>
       </div>
       <div className="products__container">
-        {products.map((product) => {
+        {filter.map((product) => {
           return <Product key={product.id} product={product} />;
         })}
       </div>
