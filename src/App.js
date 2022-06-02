28baa735e16964a28a4f2340b7a690f1f6c88768
sergen demir <dmrsrgn@gmail.com>
2022-06-02 13:37:41 -0500

    add roboto as font family and fix some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "@fontsource/roboto";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
